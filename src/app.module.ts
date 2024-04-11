716866c593e3ad0242fe49af77b3dfd8bea8afdc
Prince Narteh <prinarteh@gmail.com>
2024-04-11 20:02:11 +0000

    feat: add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TagModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
